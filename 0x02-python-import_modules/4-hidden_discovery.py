65ee6beda579bd9bf7b02a8fd09258543e798cd8
Wendy Aurellia <wendyaurellia@gmail.com>
2022-12-09 03:46:01 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
-            print(name)
+    file = dir(hidden_4)
+    length = len(file)
+    for i in range(0, length):
+        if file[i][0:2] != "__":
+            print(file[i])
