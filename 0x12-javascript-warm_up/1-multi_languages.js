a255f47953238e60be42ed7bd713aee9d8a66d7a
Wendy Aurellia <wendyaurellia@gmail.com>
2023-03-19 10:04:16 -0700

    java warm

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const cString = 'C is fun';
-const pyString = 'Python is cool';
-const jsString = 'JavaScript is amazing';
+const cLang = 'C is fun';
+const pythonLang = 'Python is cool';
+const javaSceiptLang = 'JavaScript is amazing';
 
-console.log(cString);
-console.log(pyString);
-console.log(jsString);
+console.log(cLang);
+console.log(pythonLang);
+console.log(avaScriptlang);
